df9746ddd5ade8ed9946bacc6066a8ce64959bf1
RENARD Laurent <laurent34azerty@gmail.com>
2019-12-10 12:24:23 -0500

    extenelize reporters (#54)

--- rollup/build.js
+++ rollup/build.js
@@ -1,3 +1,6 @@
+import node from 'rollup-plugin-node-resolve';
+import cjs from 'rollup-plugin-commonjs';
+
 export default {
     input: './src/index.js',
     output: [{
@@ -6,6 +9,9 @@ export default {
     }, {
         file: './dist/bundle/index.mjs',
         format: 'es'
+    }, {
+        file: './dist/bundle/module.js',
+        format: 'es'
     }],
-    plugins: []
+    plugins: [node(), cjs()]
 };
