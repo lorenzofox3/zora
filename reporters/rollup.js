0f534127c534add92b583bca35351295c48922ae
Laurent RENARD <laurent34azerty@gmail.com>
2021-05-18 10:31:01 +0200

    remove dependencies from bundle

--- reporters/rollup.js
+++ reporters/rollup.js
@@ -1,13 +1,14 @@
-import node from '@rollup/plugin-node-resolve';
-
 export default {
-    input: './src/index.js',
-    output: [{
-        file: './dist/index.cjs',
-        format: 'cjs'
-    }, {
-        file: './dist/index.js',
-        format: 'es'
-    }],
-    plugins: [node()]
+  input: './src/index.js',
+  output: [
+    {
+      file: './dist/index.cjs',
+      format: 'cjs',
+    },
+    {
+      file: './dist/index.js',
+      format: 'es',
+    },
+  ],
+  plugins: [],
 };
