f4e227583f07410eaea5ee41f7d43f583d787677
Laurent RENARD <laurent34azerty@gmail.com>
2022-06-16 15:35:46 +0200

    fix descriptions

--- pta/test/samples/serial/b.spec.js
+++ pta/test/samples/serial/b.spec.js
@@ -2,7 +2,7 @@ import { test } from 'zora';
 import { setTimeout } from 'node:timers/promises';
 import { counter } from './shared-state.js';
 
-await test(`first one`, async (t) => {
+await test(`a second one`, async (t) => {
   await setTimeout(200);
   counter.value += 1;
   t.eq(counter.value, 2);
--- pta/test/samples/serial/c.spec.js
+++ pta/test/samples/serial/c.spec.js
@@ -2,7 +2,7 @@ import { test } from 'zora';
 import { setTimeout } from 'node:timers/promises';
 import { counter } from './shared-state.js';
 
-await test(`first one`, async (t) => {
+await test(`a third one`, async (t) => {
   await setTimeout(100);
   counter.value += 1;
   t.eq(counter.value, 3);
